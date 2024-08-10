cc0258898091c4ddbd9068e26fc5390f84fcc454
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-08-10 16:22:39 -0600

    Create Austins Coding Challenge.c

---
+++ 2024/Austins coding challenge/Austins Coding Challenge.c
@@ -0,0 +1,48 @@
+/**Austins Coding Challenge.c
+ * by Nathan Pelletier
+ * August 10, 2024
+ * 
+ * Original coding idea:
+ * https://youtu.be/M8C8dHQE2Ro
+ * 
+ * Original code to convert:
+ * https://github.com/arhourigan/graveler/blob/main/graveler.py
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define Max 1000000000
+#define NumTurns 231
+
+
+int roll(int dice){
+    return (rand() % dice) + 1;
+}
+
+int main(int argc, char* argv){
+    int max_one_rolls = 0;
+    int current_one_rolls;
+    int current_roll = 0;
+    int rolls;
+    srand(time(NULL));
+
+    for(rolls = 0; rolls < Max; rolls++){
+        if(current_one_rolls == 177){
+            break;
+        }
+        current_one_rolls = 0;
+        for(int j = 0; j < NumTurns; j++){
+            current_roll = roll(4);
+            if(current_roll == 1){
+                current_one_rolls++;
+            }
+        }
+        if(current_one_rolls > max_one_rolls){
+            max_one_rolls = current_one_rolls;
+        }
+    }
+
+    printf("Highest Ones Roll: %d \n", max_one_rolls);
+    printf("Number of Roll Sessions: %d", rolls);
+    return 0;
+}
