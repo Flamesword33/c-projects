d24a00e086395d34ff4d61b7ad71d9a568d3ac82
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-11-19 14:57:01 -0700

    Removed UofA content
    Moved to private folder

--- 2019/acctdb/child_proc_semaphore/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-umask 0000
-PATH=. ./acctdb manytrans.db ../transactions ../transactions ../transactions ../transactions ../transactions ../transactions ../transactions ../transactions ../transactions ../transactions
