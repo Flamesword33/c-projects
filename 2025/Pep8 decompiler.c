d5b272f991f80b0a78b5d1ac1b0355251db32029
Nathan Pelletier <nathantylerpelletier@gmail.com>
2025-06-04 14:49:16 -0600

    Wrote skeleton for Pep8 decompiler

--- 2025/Pep8 decompiler.c
+++ 2025/Pep8 decompiler.c
@@ -5,11 +5,30 @@
  * Takes a pepo file full of hex and translates to working assembly
  */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #define True 1
 #define False 0
 
-int main(){
-    
+FILE read_file(int argc, char **argv){
+
+}//read_file
+
+FILE interpret_object_code(FILE object_code){
+
+}//interpret_object_code
+
+int output_file(FILE assembly_code){
+
+}//output_file
+
+int main(int argc, char **argv){
+    //welp lost the code for reading a c file with a c file... 
+    FILE object_code;
+    FILE assembly_code;
+    object_code = read_file(argc, argv);
+    assembly_code = interpret_object_code(object_code);
+    output_file(assembly_code);
     return 0;
 }//main
 
