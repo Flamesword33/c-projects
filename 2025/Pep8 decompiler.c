85aac35cc9e26830795c878bffe2e14fd227d549
Nathan Pelletier <nathantylerpelletier@gmail.com>
2025-06-04 14:23:40 -0600

    Create Pep8 decompiler.c

---
+++ 2025/Pep8 decompiler.c
@@ -0,0 +1,15 @@
+/**Pep8 decompiler.c
+ * by Nathan Pelletier
+ * started June 4 2025
+ * 
+ * Takes a pepo file full of hex and translates to working assembly
+ */
+#include <stdio.h>
+#define True 1
+#define False 0
+
+int main(){
+    
+    return 0;
+}//main
+
